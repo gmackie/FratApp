9177554934b82792a209ba1e8df164cfee004f49
Christopher Heyer <cwheyer@umich.edu>
2013-12-12 17:16:07 -0500

    Fixed test cases

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,7 +7,7 @@ describe "Static pages" do
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_content('Sample App') }
+    it { should have_content('FratApp') }
     it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
 
@@ -26,16 +26,6 @@ describe "Static pages" do
         end
       end
 
-      describe "follower/following counts" do
-        let(:other_user) { FactoryGirl.create(:user) }
-        before do
-          other_user.follow!(user)
-          visit root_path
-        end
-
-        it { should have_link("0 following", href: following_user_path(user)) }
-        it { should have_link("1 followers", href: followers_user_path(user)) }
-      end
     end
   end
 
