e4e6b623bb1f2d83d29d0d455cde1aa916a7d1f0
Christopher Heyer <cwheyer@umich.edu>
2013-12-12 19:45:31 -0500

    Added comments resources to microposts

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ SampleApp::Application.routes.draw do
     end
   end
   resources :sessions,      only: [:new, :create, :destroy]
-  resources :microposts,    only: [:create, :destroy]
+  resources :microposts,    only: [:create, :destroy] do
+    resources :comments,    only: [:create, :destroy]
+  end
   resources :relationships, only: [:create, :destroy]
   root to: 'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
