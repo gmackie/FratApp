4b11a9fbeca7623dd3d1c34f7a3fa57040e7a6e2
Christopher Heyer <cwheyer@umich.edu>
2013-12-12 23:35:24 -0500

    Comments fixed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,9 @@ class StaticPagesController < ApplicationController
 
   def home
     if signed_in?
+      @junk = Micropost.all
+      @microposts = @junk.paginate(page: params[:page])
+      @comment = Comment.new
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
     end
