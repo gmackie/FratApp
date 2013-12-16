c9903942a2c20b5e6495d9d4703d1fb7afd0fbc3
Graham Mackie <mackieg@umich.edu>
2013-12-16 05:20:24 -0500

    created posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,31 @@
 class PostsController < ApplicationController
+  before_action :signed_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
+
   def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:success] = "post created!"
+      redirect_to root_url
+    else
+      @feed_items = []
+      render 'static_pages/home'
+    end
+  end
+  
+  def destroy
+    @post.destroy
+    redirect_to root_url
   end
 
-  def destroy
-  end
+  private
+
+    def post_params
+      params.require(:post).permit(:title,:content,:group_id,:user_id)
+    end
+  
+    def correct_user
+      @post = current_user.posts.find_by(id: params[:id])
+      redirect_to root_url if @post.nil?
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+  belongs_to :user    # foreign key - user_id
+  belongs_to :group   # foreign key - group_id
+  has_many :comments, dependent: :destroy
+  default_scope -> { order('updated_at DESC') }
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :user_id, presence: true
+  
+  # Returns microposts from the users being followed by the given user.
+  def self.from_users_followed_by(user)
+    followed_user_ids = "SELECT followed_id FROM relationships
+                         WHERE follower_id = :user_id"
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id", 
+          user_id: user.id)
+  end
 end
